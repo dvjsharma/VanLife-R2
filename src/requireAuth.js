99d0615f9ec3c9b55b8ee8ef12796061f204aa57
Divij Sharma <divijs75@gmail.com>
2023-09-27 04:32:09 +0530

    bug fixes #1

--- src/requireAuth.js
+++ src/requireAuth.js
@@ -2,5 +2,8 @@ import React from "react";
 
 export function requireAuth(){
     const isLoggedIn=localStorage.getItem("UserAuth")
+    if(isLoggedIn===null){
+        return "false"
+    }
     return isLoggedIn
 } 
